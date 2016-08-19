8f755bcb7f7a48705d49e49b5617353f4374bbeb
Maneesh Sahu-SSI <maneesh.sahu@ssi.samsung.com>
2016-08-19 13:13:58 -0700

    Updated tokens test

--- src/test/java/cloud/artik/api/TokensApiTest.java
+++ src/test/java/cloud/artik/api/TokensApiTest.java
@@ -3,37 +3,24 @@ package cloud.artik.api;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import cloud.artik.client.ApiClient;
 import cloud.artik.model.CheckTokenResponse;
 import cloud.artik.model.RefreshTokenResponse;
 import cloud.artik.model.TokenRequest;
 
-public class TokensApiTest {
-    //protected String basePath = "https://accounts.artik.cloud";
-
-    protected TokensApi api(String accessToken) {
-        try {
-            ApiClient client = new ApiClient();
-            client.setAccessToken(accessToken);
-            //client.setBasePath(basePath);
-            client.setDebugging(true);
-
-            TokensApi retVal = new TokensApi();
-            retVal.setApiClient(client);
-
-            return retVal;
-        } catch (Exception exc) {
-            exc.printStackTrace();
-            return null;
-        }
+public class TokensApiTest extends ArtikCloudApiTest {
+    protected TokensApi apiClient = null;
+    
+    @Before
+    public void setUp() throws Exception {
+        this.apiClient = (TokensApi) super.api(TokensApi.class, "user1.token");
     }
-
+    
     @Test
     public void testCheckToken() throws Exception {
         String userAccessToken = "6907fe156b6848fa82395b2b99a882ed";
-        TokensApi apiClient = api(userAccessToken);
 
         TokenRequest request = new TokenRequest();
         request.setToken(userAccessToken);
@@ -45,9 +32,7 @@ public class TokensApiTest {
 
     @Test
     public void testRefreshToken() throws Exception {
-        String userAccessToken = "6907fe156b6848fa82395b2b99a882ed";
         String refreshToken = "bb90333aee114b3e97284814d978080d";
-        TokensApi apiClient = api(userAccessToken);
 
         RefreshTokenResponse refreshTokenResponse = apiClient.refreshToken(
                 "refresh_token", refreshToken);
