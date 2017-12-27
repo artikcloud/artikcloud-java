1b50a36ba814025f30922842b2db434cdc026d57
jonathanpaek <j.paek1@samsung.com>
2017-12-26 17:39:10 -0800

    DX-1505 refactor test using retrofit2 for SendMessageGetMessageTest.java

--- src/test/java/cloud/artik/integration/SendGetMsgTest.java
+++ src/test/java/cloud/artik/integration/SendGetMsgTest.java
@@ -19,105 +19,99 @@ package cloud.artik.integration;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import cloud.artik.client.ApiClient;
-import cloud.artik.client.ApiException;
-import cloud.artik.client.Configuration;
-import cloud.artik.client.auth.*;
 import cloud.artik.config.Config;
 import cloud.artik.model.Message;
 import cloud.artik.model.MessageIDEnvelope;
 import cloud.artik.model.NormalizedMessage;
 import cloud.artik.model.NormalizedMessagesEnvelope;
+import retrofit2.Response;
 import cloud.artik.api.MessagesApi;
 
 /**
- * This test case tests the basic mqtt functions using Junit framework. 
- * It is disabled by default. Consult the instruction below to enable and run the tests
- * 
- * How to run the tests of this test case
- * 
- * Follow the instruction in config/Config.java:
- *  Fill in device id/token for "Demo Fire Sensor"
- *  Run the test case: "mvn clean -Dtest=SendGetMsgTest test"
- *    
+ * Follow the instruction in config/Config.java: Fill in device id/token for
+ * "Demo Fire Sensor" you added into your account Run the test case:
+ * "mvn clean -Dtest=SendGetMsgTest test"
  */
 public class SendGetMsgTest {
-    // Device of "Demo Fire Sensor"
-    private String deviceId     = null;
-    private String deviceToken  = null;
 
-    private OAuth artikcloud_oauth = null;
+    // credentials for making api call for this device
+    private String deviceId = null;
+    private String deviceToken = null;
+
+    // random value sent to device
     private int tempValueSent = 50;
 
-	@Before
-	public void setUp() {
-		deviceId = Config.fireSensorDeviceId;
-		assertNotNull(deviceId);
-		assertFalse(deviceId.isEmpty());
-		
-		deviceToken = Config.fireSensorDeviceToken;
-		assertNotNull(deviceToken);
-		assertFalse(deviceToken.isEmpty());
+    // client for initializing Apis
+    private ApiClient apiClient = null;
 
-        // Configure OAuth2 access token for authorization: artikcloud_oauth
-		ApiClient defaultClient = Configuration.getDefaultApiClient();
-        artikcloud_oauth = (OAuth) defaultClient.getAuthentication("artikcloud_oauth");
-        assertNotNull(artikcloud_oauth);
-        artikcloud_oauth.setAccessToken(deviceToken);
-	}
-	
-	@Test
-	public void sendGetMsgSync() {
-        // instantiate the MessageAPI to send and receive messages
-        MessagesApi messageApiInstance = new MessagesApi();
+    @Before
+    public void setUp() {
+        deviceId = Config.fireSensorDeviceId;
+        deviceToken = Config.fireSensorDeviceToken;
+        apiClient = new ApiClient("artikcloud_oauth");
+        apiClient.setAccessToken(deviceToken);
+    }
 
-        // message object that is passed in the body
-        Message data = new Message(); 
+    @Test
+    public void sendGetMsgSync() {
+
+        // reference messagesApi
+        MessagesApi messagesApi = apiClient.createService(MessagesApi.class);
+
+        // prepare message object to send
+        Message data = new Message();
         data.setSdid(deviceId);
-         
-        // prepare a random temp value to send to our demo sensor
-        Map <String, Object> myData = new HashMap<String, Object>();
-        myData.put("temp", tempValueSent);
 
+        // prepare a temp value to send
+        Map<String, Object> myData = new HashMap<String, Object>();
+        myData.put("temp", tempValueSent);
         data.setData(myData);
-       
-        // Now lets send our data. You should receive a message id (mid) in a successful response.
+
+        // make a synchronous api call. expect a 'mid' value in response.
         try {
-            MessageIDEnvelope result = messageApiInstance.sendMessage(data);
-            assertNotNull(result.getData().getMid());
-//            System.out.println(result);
-        } catch (ApiException e) {
+            Response<MessageIDEnvelope> response = messagesApi.sendMessage(data).execute();
+            assertTrue(response.isSuccessful());
+            assertNotNull(response.body().getData().getMid());
+        } catch (IOException e) {
+            // IOException thrown for 401/403 Unauthorized/Forbidden error.
             System.err.println("Exception when calling MessagesApi#sendMessage");
             e.printStackTrace();
             fail();
+        } catch (Exception e) {
+            fail();
         }
-        
-        // Now let's retrieve our data. 
+
+        // Now let's retrieve our data.
         try {
-            NormalizedMessagesEnvelope normalizedMessagesEnvelope =
-            		messageApiInstance.getLastNormalizedMessages(1, deviceId, null);
-            
-            //each message is wrapped in NormalizedMessage object.  
-            List<NormalizedMessage> messages = normalizedMessagesEnvelope.getData();
+
+            // prepare parameters for request
+            int count = 1;
+            String sdids = deviceId;
+            String fieldPresence = null;
+            Response<NormalizedMessagesEnvelope> response = messagesApi
+                    .getLastNormalizedMessages(count, sdids, fieldPresence).execute();
+
+            List<NormalizedMessage> messages = response.body().getData();
             assertFalse(messages.isEmpty());
-//            for(NormalizedMessage message: messages) {
-//            	System.out.println(message);
-//            }
-            
-            // Get message content
+
+            // Verify last message sent to device located at index 0
             NormalizedMessage msg = messages.get(0);
-            int tempValueGet = ((Double)msg.getData().get("temp")).intValue();
+            int tempValueGet = ((Double) msg.getData().get("temp")).intValue();
             assertEquals(tempValueSent, tempValueGet);
-        } catch (ApiException e) {
+        } catch (IOException e) {
+            // IOException thrown for 401/403 Unauthorized/Forbidden error.
             System.err.println("Exception when calling MessagesApi#getLastNormalizedMessages");
             e.printStackTrace();
             fail();
         }
-        
+
     }
 
 }
